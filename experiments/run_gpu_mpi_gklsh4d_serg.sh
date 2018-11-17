32df021ced7da81bc3da8ca3f1d181f82fe4139b
Semen Bevzuk <semen.bevzuk@gmail.com>
2018-11-17 20:33:14 +0300

    Update all run_gpu_mpi like run_gpu_mpi_gklss4d_serg

--- experiments/run_gpu_mpi_gklsh4d_serg.sh
+++ experiments/run_gpu_mpi_gklsh4d_serg.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 #SBATCH --time=36:00:00
 #SBATCH --partition=gpu
-#SBATCH -N 12 
+#SBATCH -N 12
 
-srun -n 1 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_1_1_01.json --delay 0.1
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_1_01.json --delay 0.1
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_16_01.json --delay 0.1
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_1_01.json --delay 0.1
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_16_01.json --delay 0.1
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_1_01.json --delay 0.1
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_16_01.json --delay 0.1
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_1_01.json --delay 0.1
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_16_01.json --delay 0.1
+declare -a n_nodes=("1" "2" "4" "8" "12")
+declare -a delays=("0.1" "0.5" "1")
 
-srun -n 1 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_1_1_05.json --delay 0.5
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_1_05.json --delay 0.5
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_16_05.json --delay 0.5
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_1_05.json --delay 0.5
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_16_05.json --delay 0.5
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_1_05.json --delay 0.5
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_16_05.json --delay 0.5
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_1_05.json --delay 0.5
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_16_05.json --delay 0.5
+eps=0.031622776601683
+iters=250000
+class="gklsH"
+folder="gpu_mpi/gklsh4d_serg"
 
-srun -n 1 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_1_1_1.json --delay 1
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_1_1.json --delay 1
-srun -n 2 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_2_16_1.json --delay 1
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_1_1.json --delay 1
-srun -n 4 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_4_16_1.json --delay 1
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_1_1.json --delay 1
-srun -n 8 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_8_16_1.json --delay 1
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 1 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_1_1.json --delay 1
-srun -n 12 ../build/bin/mpi_example -s -a 0.031622776601683 -e 250000 -p 16 -d 4 -c gklsH -f gpu_mpi/gklsh4d_serg/midaco_12_16_1.json --delay 1
+for delay in "${delays[@]}"
+do
+  delay_str=${delay//[.]/}
+  for n_num in "${n_nodes[@]}"
+  do
+    srun -n $n_num ../build/bin/mpi_example -s -a $eps -e $iters -p 1 -d 4 -c $class -f $folder/midaco_"$n_num"_1_$delay_str.json --delay $delay
+    srun -n $n_num ../build/bin/mpi_example -s -a $eps -e $iters -p 16 -d 4 -c $class -f $folder/midaco_"$n_num"_16_$delay_str.json --delay $delay
+  done
+done
