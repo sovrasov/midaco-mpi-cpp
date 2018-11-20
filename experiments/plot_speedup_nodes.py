fd4de9b8b650cbcd8706c2a4e851a8ac0ee21410
SemyonBevzuk <semen.bevzuk@gmail.com>
2018-11-20 18:20:45 +0300

    Update plot_speedup_nodes (#4)
    * Add mpi log
    
    * Add scripts to mpi_example
    
    * Add log mpi_example
    
    *  Add plot_speedup_nodes
    
    * Update plot_speedup_nodes for threads
    
    * Update all run_gpu_mpi like run_gpu_mpi_gklss4d_serg
    
    *  Fix plot_speedup_nodes
    
    * Updat results in experiments
    
    * Add generate_table_mpi
    
    * Delete json, csv, pdf from gpu_mpi
    
    * Fix experiments
    
    * Add experiments
    
    * Update update_plots

---
+++ experiments/plot_speedup_nodes.py
@@ -0,0 +1,75 @@
+import argparse
+import json
+import os
+
+import matplotlib.pyplot as plt
+
+def get_single_node_time(file_folder, delay):
+    file_name = 'midaco_1_1' + str(delay) + '.json'
+    data = json.load(open(os.path.join(file_folder, file_name)))
+    return data['avg_time']
+
+def get_multi_node_time(file_folder, threads, delay = ''):
+    avg_time = []
+    for i in [2, 4, 8, 12]:
+        file_name = 'midaco_' + str(i) + str(threads) + str(delay) + '.json'
+        data = json.load(open(os.path.join(file_folder, file_name)))
+        avg_time.append(data['avg_time'])
+    return avg_time
+
+def add_line(single_thread_time, multi_threaded_time, thread, delay):
+    x = [2, 4, 8, 12]
+    y= [single_thread_time/multi_threaded_time[i] for i in range(0, 4)]
+    lable = ''
+    if delay == '_01':
+        lable = 'delay = 0.1'
+    elif delay == '_05':
+        lable = 'delay = 0.5'
+    elif delay == '_1':
+        lable = 'delay = 1'
+
+    if thread == '_1':
+        lable += ' threads = 1'
+    elif thread == '_16':
+        lable += ' threads = 16'
+
+    plt.plot(x, y, 'o-', label = lable)
+
+
+def plot_speedup_nodes(stats_folder, nodes, threads, delays, show=True, filename=None,):
+    for delay in delays:
+        single_node_time = get_single_node_time(stats_folder, delay)
+        for thread in threads:
+            multi_node_time = get_multi_node_time(stats_folder, thread, delay)
+            add_line(single_node_time, multi_node_time, thread, delay)
+
+    plt.legend()
+    plt.xlabel(r'$Nodes$') 
+    plt.ylabel(r'$Speedup$') 
+    plt.title(r'$midaco$')
+    plt.grid(True)
+
+    if filename:
+        plt.savefig(filename, dpi=300)
+
+    if show:
+        plt.show()
+
+def main(args):
+    nodes = ['_1', '_2', '_4', '_8', '_12']
+    threads = ['_1', '_16']
+    delays = ['_01', '_05', '_1']
+    filename = 'speedup_nodes.pdf'
+    try:
+        plot_speedup_nodes(args.stats_folder, nodes, threads, delays,
+                    show = args.show, filename = os.path.join(args.stats_folder, filename))
+    except BaseException as exception:
+        print(exception)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('stats_folder', type=str)
+    parser.add_argument('--show', action='store_true')
+    parser.add_argument('--output', type=str, default='')
+
+    main(parser.parse_args())
